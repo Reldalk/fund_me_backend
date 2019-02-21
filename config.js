30abe790d859c5ad4bb534cc833d8d510d0f1bfc
Ubuntu <ubuntu@ip-172-31-47-153.us-east-2.compute.internal>
2019-02-21 04:58:14 +0000

    server transfer

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 module.exports = {
-  PORT: process.env.PORT || 8080,
+  PORT: process.env.PORT || 3000,
   DATABASE_URL: process.env.DATABASE_URL,
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
